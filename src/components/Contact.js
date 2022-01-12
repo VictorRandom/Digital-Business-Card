d30529277e1e4caf6496885ec9194007473543b3
Victor Miranda Balduino <victormbalduino@hotmail.com>
2022-01-12 13:11:48 -0300

    botao email, direcionar ao meu email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import { FaEnvelope } from 'react-icons/fa';
-import './Contact.css';
+import React from "react";
+import { FaEnvelope } from "react-icons/fa";
+import "./Contact.css";
 
 const Contact = () => {
-    return(
-            <button id="contact"><FaEnvelope id="logo" />  Email</button>
-    );
+  return (
+    <button id="contact">
+      <a
+        style={{ textDecoration: "none", color: "black" }}
+        href="mailto:victormbalduino@hotmail.com"
+      >
+        <FaEnvelope id="logo" /> Email
+      </a>
+    </button>
+  );
 };
 
 export default Contact;
